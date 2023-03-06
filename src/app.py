7bc410c2d05c86bbe5cc68f9f33652f9390696e6
tvs_senthil <senthilkumarimuthu@gmail.com>
2023-03-06 13:53:08 +0530

    logging format

--- src/app.py
+++ src/app.py
@@ -1,11 +1,27 @@
 # Simple flask app to test logging
 
 from flask import Flask, request, render_template
-from utils.logging.custom_logging import logger
+from utils.logging.custom_logging import logger,ch
+import logging
+import os
+os.environ["WERKZEUG_RUN_MAIN"] = "true"  # removes message 'started serving APP'
 
+
+def setup_loging():
+
+    """Remove werkzeug logging handler and add custom logging handler"""
+    flask_logger = logging.getLogger('werkzeug')  # grabs underlying WSGI logger
+    for _handler in flask_logger.handlers:
+        flask_logger.removeHandler(_handler)
+    flask_logger.name = logger.name
+    flask_logger.addHandler(ch)
+
+
+setup_loging()
 
 logger.debug('Starting Flask APP')
 app = Flask(__name__)
+app.logger.disabled = True
 
 
 @app.route("/")
@@ -16,6 +32,7 @@ def home():
     logger.critical('Checking CRITICAL message')
     logger.error('Checking ERROR message')
     return "Hello World!"
+
 
 # handling CORS
 @app.after_request
--- src/utils/logging/custom_logging.py
+++ src/utils/logging/custom_logging.py
@@ -1,5 +1,5 @@
 import logging
-
+FORMATTER = "%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s"
 # create logger
 logger = logging.getLogger('Logging_for_python')
 logger.setLevel(logging.DEBUG)
@@ -9,7 +9,7 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 # create formatter
-formatter = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:%(lineno)d — %(message)s")
+formatter = logging.Formatter(FORMATTER)
 
 # add formatter to ch
 ch.setFormatter(formatter)
